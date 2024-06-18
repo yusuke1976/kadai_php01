9e0e4017a3dc08dbe5460de0cafa9fc4dd100819
yusuke1976 <168104566+yusuke1976@users.noreply.github.com>
2024-06-18 20:33:11 +0900

    Add files via upload

---
+++ write.php
@@ -0,0 +1,35 @@
+<?php
+
+$worry = $_GET['worry'];
+$bookName = $_GET['bookName'];
+$reason = $_GET['reason'];
+
+// ファイルに書き込み
+$time = date('Y-m-d H:i:s');
+//文字作成
+$data = $time . "/" . $worry . "/" . $bookName . "/" . $reason . "\n";
+
+file_put_contents('data/data.txt',$data,FILE_APPEND);
+
+?>
+
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>File書き込み</title>
+</head>
+
+<body>
+
+    <h1>書き込みしました。</h1>
+    <h2>./data/data.txt を確認しましょう！</h2>
+
+    <ul>
+        <li><a href="read.php">確認する</a></li>
+        <li><a href="index.php">戻る</a></li>
+    </ul>
+</body>
+
+</html>
